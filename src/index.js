528549e1581d47e4f761852f2c10683b21a65d5e
DanielToledo-99 <toledo992905@gmail.com>
2021-03-25 19:55:21 -0500

    Modificacion del cuerpo del compomente App

--- src/index.js
+++ src/index.js
@@ -1,40 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-
-
-
-const Content = ... {
-  return (
-    <div>
-      <Part .../>
-      <Part .../>
-      <Part .../>
-    </div>
-  )
-}
-
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  const Content = Content => {
+    return(
+      <div>
+        <part1 />
+        <part2 />
+        <part3 />
+      </div>
+    )
   }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
   return (
     <div>
-      ...
+      <Header course={course} />
+      <Content Content={Content} />
+      <Total exercises5={exercises1 + exercises2 + exercises3} />
     </div>
   )
 }
-
 
 ReactDOM.render(<App />, document.getElementById('root'))
