fe36e4fa2c859fc611885654e662f863d1d3440b
DanielToledo-99 <toledo992905@gmail.com>
2021-03-25 19:41:10 -0500

    Modificacion del cuerpo del compomente App

--- src/index.js
+++ src/index.js
@@ -16,21 +16,6 @@ const App = () => {
       <Total ... />
     </div>
   )
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
-}
+
 
 ReactDOM.render(<App />, document.getElementById('root'))
